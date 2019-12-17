daf1af1af3256e027a19e69facd1ca15a356372a
Matheus Carmo <mematheuslc@gmail.com>
2019-12-17 13:25:50 -0300

    Add transaction use case test

--- internal/transaction/authorize_use_case_test.go
+++ internal/transaction/authorize_use_case_test.go
@@ -30,7 +30,79 @@ func TestAuthorizeViolation(t *testing.T) {
 
 	violations := useCase.Execute()
 
-	if len(violations) == 2 {
-		t.Errorf("lorem")
+	if len(violations) != 1 {
+		t.Errorf("Expected just one violations and get %v", len(violations))
+	}
+
+	if violations[0] != InsuficientLimit {
+		t.Errorf("Expected %v as violation. Get %v", InsuficientLimit, violations[0])
+	}
+}
+
+func TestDoubleTransactionViolationUseCase(t *testing.T) {
+	acStore := memorystore.NewStorage("account")
+	tStore := memorystore.NewStorage("transaction")
+
+	accountRepo := ac.Repository{DB: &acStore}
+	transactionRepo := Repository{DB: &tStore}
+
+	account := ac.Account{ActiveCard: true, AvailableLimit: 1000}
+
+	trDone := Transaction{Merchant: "nuevo-store", Amount: 150}
+	trDoubled := Transaction{Merchant: "nuevo-store", Amount: 150}
+
+	accountRepo.CreateAccount(account)
+	transactionRepo.Append(trDone)
+
+	useCase := AuthorizeUseCase{
+		accountRepo:       accountRepo,
+		transactionRepo:   transactionRepo,
+		transactionIntent: trDoubled,
+	}
+
+	violations := useCase.Execute()
+
+	if len(violations) != 1 {
+		t.Errorf("Expected just one violations and get %v", len(violations))
+	}
+
+	if violations[0] != DoubledNotAllowed {
+		t.Errorf("Expected %v as violation. Get %v", DoubledNotAllowed, violations[0])
+	}
+}
+
+func TestMoreThanAllowedTransactionViolationUseCase(t *testing.T) {
+	acStore := memorystore.NewStorage("account")
+	tStore := memorystore.NewStorage("transaction")
+
+	accountRepo := ac.Repository{DB: &acStore}
+	transactionRepo := Repository{DB: &tStore}
+
+	account := ac.Account{ActiveCard: true, AvailableLimit: 1000}
+
+	trNuevo := Transaction{Merchant: "nuevo-store", Amount: 150}
+	trCarmel := Transaction{Merchant: "carmel-store", Amount: 150}
+	trNubank := Transaction{Merchant: "nubank-store", Amount: 150}
+	trExceded := Transaction{Merchant: "intent-store", Amount: 150}
+
+	accountRepo.CreateAccount(account)
+	transactionRepo.Append(trNuevo)
+	transactionRepo.Append(trCarmel)
+	transactionRepo.Append(trNubank)
+
+	useCase := AuthorizeUseCase{
+		accountRepo:       accountRepo,
+		transactionRepo:   transactionRepo,
+		transactionIntent: trExceded,
+	}
+
+	violations := useCase.Execute()
+
+	if len(violations) != 1 {
+		t.Errorf("Expected just one violations and get %v", len(violations))
+	}
+
+	if violations[0] != MoreThanAllowed {
+		t.Errorf("Expected %v as violation. Get %v", MoreThanAllowed, violations[0])
 	}
 }
